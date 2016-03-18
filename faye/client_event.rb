717d29bb3b924a5b3c273a7d676bdcd774989b0c
Djiffit <djiffit@gmail.com>
2016-03-18 17:11:32 +0200

    update

--- faye/client_event.rb
+++ faye/client_event.rb
@@ -1,7 +1,15 @@
 class ClientEvent
+  MONITORED_CHANNELS = [ '/meta/subscribe', '/meta/disconnect', '/meta/message' ]
+
   def incoming(message, callback)
     puts message.inspect
+    return callback.call(message) unless MONITORED_CHANNELS.include? message['channel']
 
+    faye_client.publish('/messages/new', { 'message' => { 'content' => "An event happened!"} })
     callback.call(message)
+  end
+
+  def faye_client
+    @faye_client ||= Faye::Client.new('http://omnistrimfaye.herokuapp.com/faye')
   end
 end
